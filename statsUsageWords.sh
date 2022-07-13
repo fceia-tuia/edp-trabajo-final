9b09a28a82ccbe0a5365bac9d5515eabc3fa6085
Federico Antuña <federicoantuna@hotmail.com>
2022-07-13 18:39:08 -0300

    Added statsUsageWords.sh script

---
+++ statsUsageWords.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TEXT=${1,,}
+declare -A WORDS
+
+for WORD in $TEXT
+do
+    CHARACTER_COUNT=`echo -n $WORD | wc -m`
+
+    if [ $CHARACTER_COUNT -ge 4 ]; then
+            WORDS[$WORD]=$((${WORDS[$WORD]} + 1))
+    fi
+done
+
+for WORD in ${!WORDS[@]}
+do
+    echo "$WORD ${WORDS[$WORD]}"
+done | sort -rn -k2 | head -n 10 | cut -d " " -f1
+
+exit 0
