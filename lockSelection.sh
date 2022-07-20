6225e3e523bae453934481a999cdae0ecef5e2b3
Federico Antuña <federicoantuna@hotmail.com>
2022-07-20 14:35:46 -0300

    Added script for lock selection

---
+++ lockSelection.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Select by sentence (O) or paragraph (P)? "
+read SELECTION
+
+SELECTION=$(echo $SELECTION | tr '[:lower:]' '[:upper:]')
+
+# [[ $SELECTION -ne "O" || $SELECTION -ne "P" ]] && exit 1
+TEXT=$(cat test.txt)
+ELEMENTS=()
+case $SELECTION in
+    [Oo])
+        DELIMITER='. '
+        TEXT_COPY=$TEXT$DELIMITER
+        while [[ $TEXT_COPY ]]; do
+            ELEMENTS+=("${TEXT_COPY%%"$DELIMITER"*}");
+            TEXT_COPY=${TEXT_COPY#*"$DELIMITER"};
+        done;
+        ;;
+    [Pp])
+        readarray -t ELEMENTS <<< $TEXT
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+echo "Position? "
+read POSITION
+
+[[ ! $POSITION =~ ^[0-9]+$ ]] && exit 1
+
+[[ $POSITION -ge ${#ELEMENTS[@]} ]] && exit 1
+
+echo ${ELEMENTS[$POSITION]}
