2cfd73a0d63a4dd95ad6c9f449b3a10215a4abee
Leandro <leandro@DESKTOP-USTVEAJ.localdomain>
2022-07-20 18:54:00 -0300

    ejercicio-6-caseConverter Improved to take a file path as a parameter instead of a string
    This commit updates caseConverter.sh to take a text file as a parameter and returning an output.txt file with the converted text.

--- caseConverter.sh
+++ caseConverter.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-WORD=$1
-WORDCHANGED=''
+FILE=$1
+WORDCHANGED=""
 
-for (( i=0; i<${#WORD}; i++ )) ; do
-    
-    if [[ ${WORD:$i:1} =~ ^[A-ZÄËÏÖÜÁÉÍÓÚÂÊÎÔÛÀÈÌÒÙÑ]+$ ]]
-    then
-        WORDCHANGED+=$(echo ${WORD:$i:1} | tr '[:upper:]' '[:lower:]')
-        
-    elif  [[ ${WORD:$i:1} =~ ^[a-zäëïöüáéíóúáéíóúâêîôûàèìòùñ]+$ ]]
-    then
-        WORDCHANGED+=$(echo ${WORD:$i:1} | tr '[:lower:]' '[:upper:]')
-    
-    else
-        echo "It isn't a letter: " ${WORD:$i:1} 
-    fi
+while read -ra LINE; 
+do
+    SENTENCE=""
+    for WORD in "${LINE[@]}";
+    do
+        WORDCHANGED="" 
+        for (( i=0; i<${#WORD}; i++ )) ; do
+            if [[ ${WORD:$i:1} =~ ^[A-ZÄËÏÖÜÁÉÍÓÚÂÊÎÔÛÀÈÌÒÙÑ]+$ ]]
+            then
+                WORDCHANGED+=$(echo ${WORD:$i:1} | tr '[:upper:]' '[:lower:]')
+            elif  [[ ${WORD:$i:1} =~ ^[a-zäëïöüáéíóúáéíóúâêîôûàèìòùñ]+$ ]]
+            then
+                WORDCHANGED+=$(echo ${WORD:$i:1} | tr '[:lower:]' '[:upper:]')
+            else
+                WORDCHANGED+=${WORD:$i:1}                
+            fi
+        done
 
-done
+        SENTENCE+="$WORDCHANGED "
+    done;
+    echo $SENTENCE >> output.txt
+done < $FILE 
 
-echo $WORDCHANGED
