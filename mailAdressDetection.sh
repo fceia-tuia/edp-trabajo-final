eb48a145ad686e15d4d3d738c2beda85b3d69095
Leandro <leandro@DESKTOP-USTVEAJ.localdomain>
2022-07-26 13:51:27 -0300

    ejercicio-13-mailDectection Added documentation
    This commit adds the documentation to the mailAdressDetection.sh file.

--- mailAdressDetection.sh
+++ mailAdressDetection.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+### This script takes the path of the text file to be procesed as a parameter and returns a list
+### of strings consisting of the words that matches with the description of an email adress. To 
+### achive this, the file is inserted in a while loop to iterate through its lines and a for loop 
+### to iterate over the words. Then we check if the word alredy is in the list previously defined.   
+### If it isn't we check if the word ends in a period or a comma and if so, the character is removed.
+### Then the word is compared with a regular expression. If the word matches the regex, it will be 
+### added to the array previously defined. After serch for the entire text file the array is sorted  
+### alphabetically. Finally the array containing only email adresses is printed in console.
+
 FILE=$1
 
 MAIL_ADRESS=()
