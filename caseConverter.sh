7d78c5c500c090a7f3b8855020863f6b89247d11
florenciafernandez <florencia.fernandez.051@gmail.com>
2022-07-26 18:22:44 -0300

    ejercicio-6-caseConverter added documentation
    this commit modifies the caseConverter.sh script to add its documentation

--- caseConverter.sh
+++ caseConverter.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+### In this script, we use a text from an enter file and read its lines.
+### Next, we evaluate every word in each line. We use an iterative structure to analyze every character of that word.
+### If the current character is an uppercase letter (with or without accentuation), 
+### then the WORDCHANGE variable will save its value but turned to lowercase and vice verse.
+### If the current character is not a letter, it won't be changed and will just be added to the WORDCHANGED variable.
+### After, we add every WORDCHANGED that we get from the current line to the SENTENCE variable that we previously defined.
+### Finally, we send a print of the sentence to an out file.
+
+
 FILE=$1
 
 while read -ra LINE; 
