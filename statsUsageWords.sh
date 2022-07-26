4efa070a2e4ff421a20bdad82a50d5da8ecda05a
Federico Antuña <federicoantuna@hotmail.com>
2022-07-26 09:52:13 -0300

    Fixed comments and stop considering numbers as words

--- statsUsageWords.sh
+++ statsUsageWords.sh
@@ -5,7 +5,8 @@
 ### Then we declare an associative array (or dictionary) and add
 ### each used word as a key with the count of how many times it
 ### appears as the value. Finally we sort the dictionary by
-### value and display the first 10 keys
+### value and display the first 10 keys. If the "word" contains
+### numbers, then we no longer consider it as word.
 
 TEXT=$(cat "$1" | tr '[:upper:]' '[:lower:]')
 
@@ -13,11 +14,10 @@ declare -A WORDS
 
 for WORD in $TEXT
 do
-    CHARACTER_COUNT=$(echo -n $WORD | wc -m)
+    [ $WORD =~ [0-9] ] && continue
+    CHARACTER_COUNT=$(echo -n $WORD | tr -d '.' | wc -m)
 
-    if [ $CHARACTER_COUNT -ge 4 ]; then
-            WORDS[$WORD]=$((${WORDS[$WORD]} + 1))
-    fi
+    [ $CHARACTER_COUNT -ge 4 ] && WORDS[$WORD]=$((${WORDS[$WORD]} + 1))
 done
 
 for WORD in ${!WORDS[@]}
