805d42058977ed466d10d72b3bfeb8f5c850ba4b
Federico Antuña <federicoantuna@hotmail.com>
2022-07-26 10:03:15 -0300

    Added documentation and simplified reading of file

--- blankLinesCounter.sh
+++ blankLinesCounter.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-TEXT=$(cat $1)
+### In this script we use grep to count (-c) how many
+### lines do not match (-v) the regex (-E) where the
+### regex matches with any line that contains non-space.
+### Meaning that it will count how many blank lines we have
 
-grep -cvE '[^[:space:]]' <<< "$TEXT"
+grep -cvE '[^[:space:]]' $1
