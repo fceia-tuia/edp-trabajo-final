70ac7ef88a8deb9f0c4d9f8c29cc57887064628a
Flor_Fernández <florencia.fernandez.051@gmail.com>
2022-07-26 20:30:46 -0300

    Apply suggestions from code review
    Co-authored-by: salvanya <78274634+salvanya@users.noreply.github.com>
    Co-authored-by: Federico Antuña <federicoantuna@hotmail.com>

--- allVowelsInWord.sh
+++ allVowelsInWord.sh
@@ -15,34 +15,31 @@ TEXT=$1
 
 declare -A WORDS_WITH_ALL_VOWELS
 
-while read -ra LINE; 
+while read -ra LINE
 do
-    for WORD in "${LINE[@]}";
+    for WORD in "${LINE[@]}"
     do    
         if [[ $WORD =~ [,.]$ ]]
         then
             WORD=${WORD::-1}
         fi 
-        # WORD=${WORD/[áÁ]/a}
-        # WORD=${WORD/[éÉ]/e}
-        # WORD=${WORD/[íÍ]/i}
-        # WORD=${WORD/[óÓ]/o}
-        # WORD=${WORD/[úÚ]/u}
 
-       if [[ $(echo $WORD | grep -i [aá] | grep -i [eé] | grep -i [ií] | grep -i [oó] | grep -i [uú]) ]]
-        then        
-                if [[ $( echo WORDS_WITH_ALL_VOWELS[$WORD]) ]]
-                then
+       if [[ $(echo $WORD | grep -i [aá] | grep -i [eé] | grep -i [ií] | grep -i [oó] | grep -i [uú]) ]]; then
+                if [[ $( echo WORDS_WITH_ALL_VOWELS[$WORD]) ]]; then
                     WORDS_WITH_ALL_VOWELS[$WORD]=$( expr ${WORDS_WITH_ALL_VOWELS[$WORD]} + 1 )
                 else
                     WORDS_WITH_ALL_VOWELS[$WORD]=1
                 fi
         fi
-    done;
+    done
 done < $TEXT
 
+PREV_IFS=$IFS
 IFS=$'\n' WORDS_WITH_ALL_VOWELS_SORTED=($(sort <<<"${!WORDS_WITH_ALL_VOWELS[*]}"))
 
-for KEY in "${WORDS_WITH_ALL_VOWELS_SORTED[@]}"; do
+for KEY in "${WORDS_WITH_ALL_VOWELS_SORTED[@]}"
+do
     echo "$KEY (${WORDS_WITH_ALL_VOWELS[$KEY]})"
 done
+
+IFS=$PREV_IFS
