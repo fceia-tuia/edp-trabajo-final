87dbe066220cad3319a592e41267727d00c58841
Leandro <leandro@DESKTOP-USTVEAJ.localdomain>
2022-07-26 13:19:20 -0300

    ejercicio-7-substringReplace Added documentation and custom output file fix.
    This commit adds the documentation to the substringReplace.sh file. In addition the last sed command was updated with the custom output file name.

--- substringReplace.sh
+++ substringReplace.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+### This script takes the path of the text file to be procesed, the substirng to search,
+### the substring to replace, and the name of the output file as parameters, and returns
+### text file with the replace done. To achive this we conform the regex adding the accentuated
+### vowels, both lowercase and uppercase, to the string that was passed as a parameter.
+### Then we use the sed command to search and replace the seach substring regex with the
+### replace string passed as a parameter. Finally we remove the non printable characters from
+### the output file.
+
+
 FILE=$1
 SEARCH_STRING=$2
 REPLACE_STRING=$3
@@ -19,4 +28,4 @@ SEARCH_STRING_REGEX="${SEARCH_STRING_REGEX/'U'/'[UÚ]'}"
 SEARCH_STRING_REGEX=$( printf $SEARCH_STRING_REGEX )
 
 sed -e "s/$SEARCH_STRING_REGEX/$REPLACE_STRING/" $FILE > $OUTPUT_FILE
-sed -i 's/[^[:print:]]//g' output.txt
+sed -i 's/[^[:print:]]//g' $OUTPUT_FILE
