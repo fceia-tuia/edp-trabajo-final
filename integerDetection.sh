7c80d6bf2c704ea0c0300791df812ee0b9152b08
Federico Antuña <federicoantuna@hotmail.com>
2022-07-26 11:22:23 -0300

    Added documentation

--- integerDetection.sh
+++ integerDetection.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+### In this script we iterate over every word
+### but we only keep the numbers. If we find integers
+### we sort them and filter out repeated ones.
+### Decimal numbers (with '.' or ',') are also recognized
+### and ignored. For instance, if we find 3.5 or 3,6
+### we consider them to be decimal numbers.
+
 TEXT=$(cat $1)
 
 for WORD in $TEXT
 do
-    if [[ $WORD =~ ^[-]?[0-9]+[.]?$ ]]; then
-        echo $WORD | tr '.' ' ' | xargs
-    fi
+    [[ $WORD =~ ^[-]?[0-9]+[.]?$ ]] && echo $WORD | tr '.' ' ' | xargs
 done | sort -h | uniq
