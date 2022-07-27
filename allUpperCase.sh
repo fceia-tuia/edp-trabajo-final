f10272578c23c5c3cbbe3950c9eae424911a0e8b
Federico Antuña <federicoantuna@hotmail.com>
2022-07-26 21:13:06 -0300

    Apply suggestions from code review
    Co-authored-by: salvanya <78274634+salvanya@users.noreply.github.com>
    Co-authored-by: Flor_Fernández <florencia.fernandez.051@gmail.com>

--- allUpperCase.sh
+++ allUpperCase.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 TEXT=$(cat $1)
 
 for WORD in $TEXT
 do
-    if [[ $WORD =~ [[:upper:]]+$ ]]; then
-        echo $WORD
-    fi
+   [[ $WORD =~ [0-9] ]] && continue
+    SANITIZED_WORD=$(echo $WORD | tr -d '.')
+    [[ $SANITIZED_WORD =~ [[:upper:]]+$ ]] && echo $SANITIZED_WORD
 done | sort | uniq
