83b9e44f51844fc9f5a135aba3405939bf6c28e4
Federico Antuña <federicoantuna@hotmail.com>
2022-07-26 21:14:32 -0300

    Added documentation

--- allUpperCase.sh
+++ allUpperCase.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+### In this script we find words that are written
+### entirely in uppercase and show them. If the "word" contains
+### numbers, then we no longer consider it as word.
+
 TEXT=$(cat $1)
 
 for WORD in $TEXT
 do
-   [[ $WORD =~ [0-9] ]] && continue
+    [[ $WORD =~ [0-9] ]] && continue
     SANITIZED_WORD=$(echo $WORD | tr -d '.')
+
     [[ $SANITIZED_WORD =~ [[:upper:]]+$ ]] && echo $SANITIZED_WORD
 done | sort | uniq
